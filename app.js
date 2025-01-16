5b3e2108fcb93bbd62aa24a7daf5d77c61729c2e
sigmalewis123 <wasj4kp0t@outlook.com>
2025-01-16 15:18:57 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HoverCard from './HoverCard';
+
+function App() {
+  return (
+    <div className="App">
+      <HoverCard 
+        title="Cool Hover Effect" 
+        content="This is a card with a cool hover effect. Hover over me to see it in action!" 
+      />
+    </div>
+  );
+}
+
+export default App;
